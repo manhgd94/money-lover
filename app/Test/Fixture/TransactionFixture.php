eecd4a1d506086ef43d2c282d5c330cf6ab7c92f
Giang Manh <giangducmanh@gmail.com>
2016-06-28 11:28:21 +0700

    general code transactions

---
+++ app/Test/Fixture/TransactionFixture.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Transaction Fixture
+ */
+class TransactionFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'wallet_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'category_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'note' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'money' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'timestamp', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'wallet_id' => 1,
+			'category_id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'note' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'money' => 1,
+			'created' => '2016-06-28 06:27:16',
+			'modified' => 1467088036
+		),
+	);
+
+}
---
+++ app/View/Transactions/add.ctp
@@ -0,0 +1,25 @@
+<div class="transactions form">
+<?php echo $this->Form->create('Transaction'); ?>
+	<fieldset>
+		<legend><?php echo __('Add Transaction'); ?></legend>
+	<?php
+		echo $this->Form->input('wallet_id');
+		echo $this->Form->input('category_id');
+		echo $this->Form->input('name');
+		echo $this->Form->input('note');
+		echo $this->Form->input('money');
+	?>
+	</fieldset>
+<?php echo $this->Form->end(__('Submit')); ?>
+</div>
+<div class="actions">
+	<h3><?php echo __('Actions'); ?></h3>
+	<ul>
+
+		<li><?php echo $this->Html->link(__('List Transactions'), array('action' => 'index')); ?></li>
+		<li><?php echo $this->Html->link(__('List Wallets'), array('controller' => 'wallets', 'action' => 'index')); ?> </li>
+		<li><?php echo $this->Html->link(__('New Wallet'), array('controller' => 'wallets', 'action' => 'add')); ?> </li>
+		<li><?php echo $this->Html->link(__('List Categories'), array('controller' => 'categories', 'action' => 'index')); ?> </li>
+		<li><?php echo $this->Html->link(__('New Category'), array('controller' => 'categories', 'action' => 'add')); ?> </li>
+	</ul>
+</div>
