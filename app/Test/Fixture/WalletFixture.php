9aa1b62a91dd02f727f05dc17209ac3eaf7070fa
Giang Manh <giangducmanh@gmail.com>
2016-06-28 10:47:43 +0700

    general code wallets

---
+++ app/Test/Fixture/WalletFixture.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Wallet Fixture
+ */
+class WalletFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'current' => array('type' => 'boolean', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'user_id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'current' => 1
+		),
+	);
+
+}
