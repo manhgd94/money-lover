40c1f3705deaeccee98059390907446d29a76663
Giang Manh <giangducmanh@gmail.com>
2016-06-29 15:41:34 +0700

    fix themed and uploadfile

--- app/Model/User.php
+++ app/Model/User.php
@@ -25,8 +25,8 @@ class User extends AppModel {
 		),
 		'username' => array(
 			'notBlank' => array(
-				'rule' => array('notBlank'),
-				//'message' => 'Your custom message here',
+				'rule' => 'isUnique',
+				'message' => 'The username has already been taken',
 				//'allowEmpty' => false,
 				//'required' => false,
 				//'last' => false, // Stop validation after this rule
@@ -57,7 +57,7 @@ class User extends AppModel {
 			'notBlank' => array(
 				'rule' => array('notBlank'),
 				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
+				// 'allowEmpty' => false,
 				//'required' => false,
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
