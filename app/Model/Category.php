224dcdab825b57b742f46a3444aa92e2f85793cc
Giang Manh <giangducmanh@gmail.com>
2016-07-07 11:56:17 +0700

    fix model Category

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -13,24 +13,14 @@ class Category extends AppModel {
  * @var array
  */
     public $validate = array(
-        'pid' => array(
-            'numeric' => array(
-                'rule' => array('numeric'),
-                'message' => 'Your custom message here',
-                //'allowEmpty' => false,
-                //'required' => false,
-                //'last' => false, // Stop validation after this rule
-                //'on' => 'create', // Limit validation to 'create' or 'update' operations
-            ),
-        ),
         'name' => array(
             'notBlank' => array(
-                'rule' => array('notBlank'),
-                //'message' => 'Your custom message here',
+                'rule'    => 'isUnique',
+                'message' => 'The username has already been taken',
                 //'allowEmpty' => false,
                 //'required' => false,
                 //'last' => false, // Stop validation after this rule
-                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+                'on'      => 'create', // Limit validation to 'create' or 'update' operations
             ),
         ),
         'type' => array(
